80d79b32209f08b98fadb93602a873fca283c2bd
anathans1 <alexnathans1993@gmail.com>
2022-05-31 20:13:43 -0400

    Finished Virtual Pet Iteration One

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,7 +1,10 @@
 package virtual_pet;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.time.Clock;
 import java.time.Duration;
+import java.util.Random;
 
 public class VirtualPet {
 
@@ -15,14 +18,40 @@ public class VirtualPet {
 
     private int boredomLevel;
 
+    private int foodInTheFridge;
+
+    private int keys;
+
+    private int phone;
+
+    private int life;
+
+
+
+    //public int getTheLifeVariableValue() {
+    //   return this.life;
+    // }
+
+
+    // public void setLife(int userSetsLife) {
+    //    this.life = userSetsLife;
+    //this makes the user able to call the user's ability to set the int and set it using rocky.setLife(4) // four being the variable set through the application
+    // }
+
+
+    // new VirtualPet("Rocky", 3, 5,5,5);
     public VirtualPet(String name, int hungerLevel, int urgeToSteal, int thirstLevel, int boredomLevel) {
         this.name = name;
         this.hungerLevel = hungerLevel;
         this.urgeToSteal = urgeToSteal;
         this.thirstLevel = thirstLevel;
         this.boredomLevel = boredomLevel;
+        //     this.foodInTheFridge = foodInTheFridge;
+        //     this.keys = keys;
+        //     this.phone = phone;
+        //     this.life = life;
     }
-
+// line 26-36 defining the way that the class is initialized
 
     public String getname() {
         return name;
@@ -44,19 +73,37 @@ public class VirtualPet {
         return boredomLevel;
     }
 
+    //TODO:
+    // public int getFoodInTheFridge() {
+    //    return foodInTheFridge;
+    // }
+    // public int getKeys() {
+    //     return keys;
+    // }
+    // public int getPhone() {
+    //    return phone;
+    // }
+    // public int life() {
+    //     return life;
+    // }
+
 
     public void greeting() {
-        System.out.println(name + "'s hunger level is " + hungerLevel + " and his thirst level is " + thirstLevel);
-        System.out.println(name + "'s thirst level is " + thirstLevel + " and his urge to steal is " + urgeToSteal);
+        System.out.print("\uD83E\uDD9D" + name + "'s" + "🦝 hunger level is " + hungerLevel + ", his thirst level is " + thirstLevel);
+        System.out.println(", his boredom level is " + boredomLevel + " and his urge to steal is " + urgeToSteal);
 
     }
 
     public void feed() {
         hungerLevel = -1;
+        //TODO:  thirstLevel = thirstLevel +2;
     }
+
     public void water() {
         thirstLevel = -1;
+        // TODO:  hungerLevel = hungerLevel + 2;
     }
+
     public void play() {
         boredomLevel = -1;
     }
@@ -65,7 +112,9 @@ public class VirtualPet {
         hungerLevel++;
         thirstLevel++;
         boredomLevel++;
-        //want to make boredom level random between 1 and 10
+        // TODO 5/25/22 add boredom level randomized
+        // TODO eventually want to do this ---> boredomLevel = new Random().nextInt(4) + 1;
+        // TODO but confused about how to make the boredom level go over 5 due to user negligence not randomization
 
         if (hungerLevel > 5) {
             urgeToSteal++;
@@ -74,30 +123,70 @@ public class VirtualPet {
             urgeToSteal++;
         }
         if (boredomLevel > 5) {
-            // TODO: 5/25/22 add boredom level randomized
-            // int thirstLevel;
-            // if (true)
+            urgeToSteal++;
         }
+        if (urgeToSteal > 9) {
+            System.out.println("You're raccoon has stolen your food, phone and keys. You are now their pet!");
+            System.out.println("                                                              _/;\n" +
+                    "                                                            _(~  ;\n" +
+                    "                                                          _(##    ;\n" +
+                    "                                    ___                 _(   ###   ;\n" +
+                    "                           __,-----'   `--.__         _( ##    ### ;\n" +
+                    "               ,-.   _,---'  ,-,      ,      `---.   (##  ###    ###\n" +
+                    "              //\\\\ -'       //~\\\\   , `           `--. ##   ###   #;\n" +
+                    "            ,'/\"\"\\`,--'~~`-'/ \"\";;  `                 `-.##   ###  ;\n" +
+                    "            (( \"__;;  ,.,.,   ~~ ; ,                  ,  `.##   ##;\n" +
+                    "            ; '~_,-.      _,-'~~`-.`        ;         `,  ; ##   ;\n" +
+                    " *(*)      ;_.-'  _ `.   ;   _    `.     - `          ,`  ;  ## ;\n" +
+                    " ***     ;;   _,-'::-.: | ,-'::-._,|       _`         `   ; _,-'\n" +
+                    " \\/ +   ; ;,-:::(0)::\\; :/(0):::::::      '               |;\n" +
+                    " ---   ;  `.:::::,-'~)   (`-.::--.:;     '           ,    : --  ----\n" +
+                    "      ;    :~`-'~     \\__\\   ~~  ~.'    ;            `   ;\n" +
+                    "     ;      `-._    . (~..) `;_,-'      ;               ;    *\n" +
+                    "     .         ;`-`._ .`--'_;           ;           _,-'   *[o]*\n" +
+                    "     ;     ;  ; `-.  `--'`-'  ;        ;\\       _,-'         !\n" +
+                    "     (    ;  ;     `-.____,--;     ;  _; `-,___(_     \\?/\n" +
+                    "      \\      `-,_     ~~~~  ;       ;;    /    __}\n" +
+                    "       `._       `-.      ,-'    ; _,' ,-',,,-'\n" +
+                    " @@@@@@@  `--,~~~~~ ;    ;     _,-'    C^c^c           @@@@@@@@@\n" +
+                    " ~~~~~~~~~__ ))(_)()\\ ___)~~~~( ____         @@@@@@@@@@@@@@@/~~~~~~~\n" +
+                    "      ,-'oo //:; :; ); o ))))))) oooo-.~~~~~~~~~~~~~~~~~~~~~\n" +
+                    "     { ooo  ()() () () o ()()()() ooooo}                     -\n" +
+                    "      `-.oo ~ ~  ~  ~ oo ~ ~ ~ ~ ooo,-'     ~~~~~      ~~~\n" +
+                    "          `-------------------------\n" +
+                    "   ~~~~                  ~~~~~               ~~~~~~~   __    ~~\n" +
+                    "\n" +
+                    "      ~~~~~~                         ~~               ~~\n" +
+                    " -------------------------------------------------------------------");
+            System.exit(0);
 
+        }
+    }
+}
+
+        // TODO:
+       /*
+        // TODO: if (foodInTheFridge == 0) {
+            System.out.println("Your racoons raided your fridge. You now have no food!");
+        }
+        // TODO: if (keys == 0) {
+            System.out.println("Your racoons have stolen your phone. You can't call for help!");
+        }
+        // TODO: if (phone == 0) {
+            System.out.println("Your racoons have stolen your keys. You are trapped!");
+        }
+         //TODO: if ((foodInTheFridge + keys + phone) == 0) {
+         // System.out.println("Your racoons have taken over your house. You are now their pet...");
+            }
+        }
     }
 
 
-    // public static Clock tick(Clock baseClock, Duration tickDuration) {
-
-    //}
+    // TODO Maybe........ public static Clock tick(Clock baseClock, Duration tickDuration) {
 
 
-    //public void water() {
-    //}
-
-   // public void play() {
-    //}
-
-}
-// private int hungerLevel(do this through passage of time tick() and or through importing the clock);
-
-
-//create another class? that details what you have in your house.
-//if the urge to steal gets above 5... you lose something
-//you only have so many things. you lose if everything is stolen
-//steal food can't eat... steal phone can't call animal control
+// TODO create another class? that details what you have in your house.
+//  if the urge to steal gets above 5... you lose something
+//   you only have so many things. you lose if everything is stolen
+//    steal food can't eat... steal phone can't call animal control
+*/
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -2,14 +2,22 @@ package virtual_pet;
 
 import java.util.Scanner;
 
+
 public class VirtualPetApplication {
+
+//TODO create a greeting like below to open the game
+// System.out.println("This is Rocky, you're new raccoon. You took him in off the street and decided to take care of him.")
+// System.out.println("Your job is to keep him fed, hydrated and occupied. If you don't... he may turn on you!")
 
     public static void main(String[] args) {
         //Interact with a VirtualPet object in this method
-        VirtualPet rocky = new VirtualPet("Rocky", 3, 5,5,5);
+        System.out.println("This is Rocky, you're new raccoon. You took him in off the street and decided to take care of him.");
+        System.out.println("Your job is to keep him fed, hydrated and occupied. If you don't... he may turn on you!");
+        VirtualPet rocky = new VirtualPet("Rocky", 5, 5,5,5);
+        // calling line 26 from the class VirtualPet...
         String userOption = "";
         Scanner input = new Scanner(System.in);
-        while (true) {
+        while (!userOption.equalsIgnoreCase("quit")) {
 
                 rocky.greeting();
                 System.out.println("Type feed/water/play");
@@ -31,12 +39,8 @@ public class VirtualPetApplication {
 
                 rocky.tick();
 
-
             }
         }
-        // while(true){
-        //    rocky.greeting();
-
     }
 
 
